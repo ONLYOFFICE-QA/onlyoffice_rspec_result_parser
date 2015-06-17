b2b863dfdd869cd7ecd4b19f26b2c66c7dee6e0d
Pavel Lobashov <shockwavenn@gmail.com>
2015-06-17 13:27:22 +0300

    add not working example

--- spec/rspec_result_parser_spec/rspec_result_parser_spec.rb
+++ spec/rspec_result_parser_spec/rspec_result_parser_spec.rb
@@ -1,9 +1,8 @@
 require 'rspec'
-
+require_relative '../../parsers/rspec_result_parser'
 describe 'My behaviour' do
 
-  it 'should do something' do
-
-    true.should == false
+  it 'result 1' do
+    ResultParser.parse_rspec_html_string(File.read('spec/rspec_result_parser_spec/rspec_examples/result_1.html'))
   end
 end
