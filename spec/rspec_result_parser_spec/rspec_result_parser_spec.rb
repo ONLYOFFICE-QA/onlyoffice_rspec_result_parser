816fa273be90fe9bbe7e6341878e69e2e4a0b65a
Pavel Lobashov <shockwavenn@gmail.com>
2015-06-17 13:32:46 +0300

    add more verification to result parser test

--- spec/rspec_result_parser_spec/rspec_result_parser_spec.rb
+++ spec/rspec_result_parser_spec/rspec_result_parser_spec.rb
@@ -3,6 +3,7 @@ require_relative '../../parsers/rspec_result_parser'
 describe 'My behaviour' do
 
   it 'result 1' do
-    ResultParser.parse_rspec_html_string(File.read('spec/rspec_result_parser_spec/rspec_examples/result_1.html'))
+    result = ResultParser.parse_rspec_html_string(File.read('spec/rspec_result_parser_spec/rspec_examples/result_1.html'))
+    expect(result).not_to be_nil
   end
 end
