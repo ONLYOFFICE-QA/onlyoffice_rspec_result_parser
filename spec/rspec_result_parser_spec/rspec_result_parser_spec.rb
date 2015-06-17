2f76ecff640eb324e17dcfc53b81e333f75489d5
Pavel Lobashov <shockwavenn@gmail.com>
2015-06-17 13:49:38 +0300

    reformat code for rubocop complaince

--- spec/rspec_result_parser_spec/rspec_result_parser_spec.rb
+++ spec/rspec_result_parser_spec/rspec_result_parser_spec.rb
@@ -1,7 +1,6 @@
 require 'rspec'
 require_relative '../../parsers/rspec_result_parser'
 describe 'My behaviour' do
-
   it 'result 1' do
     result = ResultParser.parse_rspec_html_string(File.read('spec/rspec_result_parser_spec/rspec_examples/result_1.html'))
     expect(result).not_to be_nil
