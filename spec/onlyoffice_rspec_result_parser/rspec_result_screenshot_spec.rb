27d201e78e2af87c8cb1c6d1b5524a298f431878
Pavel Lobashov <shockwavenn@gmail.com>
2017-11-20 17:43:47 +0300

    Store `Example#page_url` and `Example#screenshot` as separate fields

---
+++ spec/onlyoffice_rspec_result_parser/rspec_result_screenshot_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'My behaviour' do
+  it 'check screenshot field nil if no error' do
+    result = OnlyofficeRspecResultParser::ResultParser.parse_rspec_html_string(File.read('spec/rspec_examples/result_1.html'))
+    expect(result.describe.child.first.screenshot).to be_nil
+  end
+
+  it 'check page url field not empty' do
+    result = OnlyofficeRspecResultParser::ResultParser.parse_rspec_html_string(File.read('spec/rspec_examples/link_in_result.html'))
+    expect(result.describe.child.first.screenshot).to eq('https://nct-data-share.s3-us-west-2.amazonaws.com/screenshots/irkptCRjvigq.png')
+  end
+end
