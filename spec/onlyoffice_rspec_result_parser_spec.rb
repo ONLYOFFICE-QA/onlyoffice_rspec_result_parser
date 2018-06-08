76e0e5af8ba217c178133434094a44c9a1232546
Pavel Lobashov <shockwavenn@gmail.com>
2018-06-08 12:25:39 +0300

    Add `ResultParser#total_tests_count` parsing

--- spec/onlyoffice_rspec_result_parser_spec.rb
+++ spec/onlyoffice_rspec_result_parser_spec.rb
@@ -40,4 +40,9 @@ describe 'My behaviour' do
     result = OnlyofficeRspecResultParser::ResultParser.parse_rspec_html('spec/rspec_examples/result_1.html')
     expect(result.passed_count).to eq(1)
   end
+
+  it 'parse_rspec_html contained total_tests_count' do
+    result = OnlyofficeRspecResultParser::ResultParser.parse_rspec_html('spec/rspec_examples/failed-cases-count.html')
+    expect(result.total_tests_count).to eq(3)
+  end
 end
