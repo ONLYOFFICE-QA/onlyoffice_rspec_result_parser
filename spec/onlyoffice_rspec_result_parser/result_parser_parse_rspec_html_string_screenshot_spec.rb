8e78657e5a69e20cae6c84db15eb20ae364f43ff
Pavel Lobashov <ShockwaveNN@gmail.com>
2020-03-20 13:30:22 +0300

    Add `rubocop-rspec` support (#23)
    * Add rubocop-rspec integration
    
    * Fix RuboCop `RSpec/DescribeClass` cop
    
    * Fix RuboCop `RSpec/MultipleExpectations` cop

--- spec/onlyoffice_rspec_result_parser/result_parser_parse_rspec_html_string_screenshot_spec.rb
+++ spec/onlyoffice_rspec_result_parser/result_parser_parse_rspec_html_string_screenshot_spec.rb
@@ -2,14 +2,14 @@
 
 require 'spec_helper'
 
-describe 'My behaviour' do
+describe OnlyofficeRspecResultParser::ResultParser, '.parse_rspec_html_string' do
   it 'check screenshot field nil if no error' do
-    result = OnlyofficeRspecResultParser::ResultParser.parse_rspec_html_string(File.read('spec/rspec_examples/result_1.html'))
+    result = described_class.parse_rspec_html_string(File.read('spec/rspec_examples/result_1.html'))
     expect(result.describe.child.first.screenshot).to be_nil
   end
 
   it 'check page url field not empty' do
-    result = OnlyofficeRspecResultParser::ResultParser.parse_rspec_html_string(File.read('spec/rspec_examples/link_in_result.html'))
+    result = described_class.parse_rspec_html_string(File.read('spec/rspec_examples/link_in_result.html'))
     expect(result.describe.child.first.screenshot).to eq('https://nct-data-share.s3-us-west-2.amazonaws.com/screenshots/irkptCRjvigq.png')
   end
 end
