8b531c7b40dda10a405e02b9cbe5f243a3720a1b
Pavel Lobashov <shockwavenn@gmail.com>
2020-03-20 14:17:25 +0300

    Finally reduce all

--- spec/onlyoffice_rspec_result_parser/result_parser_spec.rb
+++ spec/onlyoffice_rspec_result_parser/result_parser_spec.rb
@@ -29,16 +29,4 @@ describe OnlyofficeRspecResultParser::ResultParser do
     result = described_class.parse_rspec_html(file)
     expect(result.pending_count).to eq(3)
   end
-
-  it 'parse_metadata without moveProgressBar' do
-    file = 'spec/rspec_examples/result_without_move_progress_bar.html'
-    result = described_class.parse_metadata(file)
-    expect(result.processing).to eq('0')
-  end
-
-  it 'parse_metadata without moveProgressBar result' do
-    file = 'spec/rspec_examples/result_without_move_progress_bar_result.html'
-    result = described_class.parse_metadata(file)
-    expect(result.processing).to eq('0')
-  end
 end
