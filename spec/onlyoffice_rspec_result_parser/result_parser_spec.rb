de7271941e360d90f942fc65f1af04de16839649
Pavel Lobashov <shockwavenn@gmail.com>
2020-03-20 13:48:15 +0300

    Reduce metric once more

--- spec/onlyoffice_rspec_result_parser/result_parser_spec.rb
+++ spec/onlyoffice_rspec_result_parser/result_parser_spec.rb
@@ -32,20 +32,23 @@ describe OnlyofficeRspecResultParser::ResultParser do
     expect(result).to eq(3)
   end
 
-  it 'check url is clickable' do
-    result = described_class.parse_rspec_html_string(link_in_result)
-    link = "<a href='https://doc-linux-autotest.teamlab.info/"\
+  describe 'link in result file' do
+    let(:result) { described_class.parse_rspec_html_string(link_in_result) }
+
+    it 'check url is clickable' do
+      link = "<a href='https://doc-linux-autotest.teamlab.info/"\
            "products/files/doceditor.aspx?fileid=2678604&new=true'>"\
            'https://doc-linux-autotest.teamlab.info/products/files/'\
            'doceditor.aspx?fileid=2678604&new=true</a>'
-    expect(result.describe.child.first.message).to include(link)
-  end
+      expect(result.describe.child.first.message).to include(link)
+    end
 
-  it 'check image is inline' do
-    result = described_class.parse_rspec_html_string(link_in_result)
-    img_string = "<img src='https://nct-data-share.s3-us-west-2.amazonaws.com/"\
-                 "screenshots/irkptCRjvigq.png' height='50%' width='50%'>"
-    expect(result.describe.child.first.message).to include(img_string)
+    it 'check image is inline' do
+      img_string = "<img src='https://nct-data-share."\
+                   's3-us-west-2.amazonaws.com/'\
+                   "screenshots/irkptCRjvigq.png' height='50%' width='50%'>"
+      expect(result.describe.child.first.message).to include(img_string)
+    end
   end
 
   it 'parse_rspec_html contained failed count' do
