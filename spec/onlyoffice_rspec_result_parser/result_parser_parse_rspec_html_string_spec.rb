290685423b170f81319ee652d0dfa3fc2f4470d3
Pavel Lobashov <shockwavenn@gmail.com>
2020-03-20 13:46:09 +0300

    Reduce some more metrics

--- spec/onlyoffice_rspec_result_parser/result_parser_parse_rspec_html_string_spec.rb
+++ spec/onlyoffice_rspec_result_parser/result_parser_parse_rspec_html_string_spec.rb
@@ -2,14 +2,19 @@
 
 require 'spec_helper'
 
-describe OnlyofficeRspecResultParser::ResultParser, '.parse_rspec_html_string' do
+describe OnlyofficeRspecResultParser::ResultParser,
+         '.parse_rspec_html_string' do
   it 'check page url field nil if no error' do
-    result = described_class.parse_rspec_html_string(File.read('spec/rspec_examples/result_1.html'))
+    file = File.read('spec/rspec_examples/result_1.html')
+    result = described_class.parse_rspec_html_string(file)
     expect(result.describe.child.first.page_url).to be_nil
   end
 
   it 'check page url field not empty' do
-    result = described_class.parse_rspec_html_string(File.read('spec/rspec_examples/result_page_url.html'))
-    expect(result.describe.child[2].page_url).to eq('https://saul.teamlab.info/products/files/doceditor.aspx?fileid=42774')
+    file = File.read('spec/rspec_examples/result_page_url.html')
+    result = described_class.parse_rspec_html_string(file)
+    expect(result.describe.child[2].page_url)
+      .to eq('https://saul.teamlab.info/products/files/'\
+             'doceditor.aspx?fileid=42774')
   end
 end
