f2374cbd9aa18c192a571d8e80428d3ab1299946
Pavel Lobashov <ShockwaveNN@gmail.com>
2020-12-29 18:01:54 +0300

    Release v0.2.0 (#48)

--- lib/onlyoffice_rspec_result_parser/version.rb
+++ lib/onlyoffice_rspec_result_parser/version.rb
@@ -3,7 +3,7 @@
 # Main namespace of gem
 module OnlyofficeRspecResultParser
   # @return [String] gem version
-  VERSION = '0.1.2'
+  VERSION = '0.2.0'
   # @return [String] gem name
   NAME = 'onlyoffice_rspec_result_parser'
 end
